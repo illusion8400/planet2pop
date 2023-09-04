4ee0cd261e9c45e4a485c5bc1059414f9c9a9c80
illusion8400 <115610976+illusion8400@users.noreply.github.com>
2023-09-04 15:02:41 -0600

    v1.1

--- app/src/main/java/com/spiffynet/planet2pop/MainActivity.java
+++ app/src/main/java/com/spiffynet/planet2pop/MainActivity.java
@@ -1,14 +1,16 @@
 package com.spiffynet.planet2pop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
+import android.view.Menu;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         mywebView.loadUrl(url);
         WebSettings webSettings = mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
+        Toast.makeText(MainActivity.this, "Loading...", Toast.LENGTH_SHORT).show();
         // Set up SwipeRefreshLayout for refreshing
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -44,6 +46,25 @@ public class MainActivity extends AppCompatActivity {
         });
 
     } // end on create
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.reload) {
+            mywebView.reload();
+            Toast.makeText(MainActivity.this, "Reload", Toast.LENGTH_SHORT).show();
+        } else if(item.getItemId() == R.id.quit) {
+            System.exit(0);
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
     @Override
     public void onBackPressed() {
         if (mywebView != null && mywebView.canGoBack()) {
